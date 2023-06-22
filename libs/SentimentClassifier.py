442fb0073ab1cad6e3a76054e8bc788af9e26f0c
Asopos <rene.lehmann@stud.uni-due.de>
2023-06-23 00:19:47 +0200

    Add: evaluation and fix f1 score

--- libs/SentimentClassifier.py
+++ libs/SentimentClassifier.py
@@ -1,4 +1,4 @@
-from transformers import BertTokenizer, BertForSequenceClassification, logging, get_linear_schedule_with_warmup
+from transformers import BertForSequenceClassification, get_linear_schedule_with_warmup
 import torch
 import numpy as np
 import datetime
@@ -6,29 +6,13 @@ import time
 from torch.optim import AdamW
 from sklearn.model_selection import KFold
 from torch.utils.data import TensorDataset, DataLoader, Subset, RandomSampler, SequentialSampler
+from sklearn.metrics import f1_score
 
 
 def flat_accuracy(preds, labels):
     pred_flat = np.argmax(preds, axis=1).flatten()
     labels_flat = labels.flatten()
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
-
-
-def flat_f1_score(preds, labels):
-    pred_flat = np.argmax(preds, axis=1).flatten()
-    labels_flat = labels.flatten()
-
-    tp = np.sum((pred_flat == 1) & (labels_flat == 1))
-    fp = np.sum((pred_flat == 1) & (labels_flat == 0))
-    fn = np.sum((pred_flat == 0) & (labels_flat == 1))
-
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-
-    f1_score = 2 * (precision * recall) / (precision + recall)
-
-    return f1_score
-
 
 def format_time(elapsed):
     """
@@ -92,7 +76,7 @@ def eval_model(
     model.to(device)
     total_eval_accuracy = 0
     total_eval_loss = 0
-    total_eval_f1score = 0
+    test_f1 = 0.0
     confusion_matrix = torch.zeros((3, 3))
     for batch in eval_dataloader:
         b_input_ids = batch[0].to(device)
@@ -108,21 +92,21 @@ def eval_model(
             logits = eval_output.logits
 
             _, preds = torch.max(logits, 1)
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+            test_f1 += f1_score(label_ids, preds, average='macro')
             for t, p in zip(b_labels.view(-1), preds.view(-1)):
                 confusion_matrix[t.long(), p.long()] += 1
         total_eval_loss += loss.item()
-        logits = logits.detach().cpu().numpy()
-        label_ids = b_labels.to('cpu').numpy()
         total_eval_accuracy += flat_accuracy(logits, label_ids)
-        total_eval_f1score += flat_f1_score(logits, label_ids)
     avg_val_accuracy = total_eval_accuracy / len(eval_dataloader)
-    avg_val_f1 = total_eval_f1score / len(eval_dataloader)
+    avg_val_f1 = test_f1 / len(eval_dataloader)
     avg_val_loss = total_eval_loss / len(eval_dataloader)
 
     validation_time = format_time(time.time() - t0)
 
     print("  Accuracy: {0:.2f}".format(avg_val_accuracy))
-    print(f"  F1 Score: {avg_val_f1}")
+    print(f"  F1 Score: {avg_val_f1:0:.2f}")
     print(confusion_matrix)
     print("  Validation Loss: {0:.2f}".format(avg_val_loss))
     print("  Validation took: {:}".format(validation_time))
@@ -178,3 +162,31 @@ def k_cross_fold_validation(dataset: TensorDataset, k=5, epochs=2, batch_size=16
             print("")
             print("Running Validation...")
             eval_model(model, validation_dataloader,training_stats, device)
+
+def evaluation(train_dataloader, validation_dataloader, epochs=2, device="cpu"):
+    training_stats = []
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased",
+        num_labels=3,
+        output_attentions=False,
+        output_hidden_states=False,
+    )
+    print(f"{len(train_dataloader)} training samples'")
+    print(f"{len(validation_dataloader)} validation samples")
+    optimizer = AdamW(model.parameters(),
+                      lr=1e-5,  # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                      eps=1e-8  # args.adam_epsilon  - default is 1e-8.
+                      )
+
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0,
+                                                num_training_steps=len(train_dataloader) * epochs)
+    optimizer.zero_grad()
+    for epoch in range(epochs):
+        print("")
+        print('======== Epoch {:} / {:} ========'.format(epoch + 1, epochs))
+        print('Training...')
+        train_model(model, train_dataloader, optimizer, scheduler, device)
+        print("")
+        print("Running Validation...")
+        eval_model(model, validation_dataloader, training_stats, device)
